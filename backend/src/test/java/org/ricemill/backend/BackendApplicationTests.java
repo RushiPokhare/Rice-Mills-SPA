e2ecfabe6c58b3dd4ce444f44ba52b8e05d602e1
RushiPokhare <rsp9725@gmail.com>
2025-11-17 12:47:47 +0530

    remove Nested folder

--- backend/src/test/java/org/ricemill/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/ricemill/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package org.ricemill.ricemillsspa;
+package org.ricemill.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RiceMillsSpaApplicationTests {
+class BackendApplicationTests {
 
     @Test
     void contextLoads() {
